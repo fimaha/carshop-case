f4a64991664095139cbd5ea29ddb6d5fb89f8468
Filippa Hansen <filippahansen@Filippas-MacBook-Pro.local>
2023-10-17 10:11:30 +0200

    added initital header

--- src/App.js
+++ src/App.js
@@ -2,12 +2,29 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const header = <nav className="nav-bar">
+    <p>Logo</p>
+    <ul>
+      <li>
+        <a href="/">Home</a>
+      </li>
+      <li>
+        <a href="contact-us">Contact</a>
+      </li>
+      <li>
+        <a href="/">Log in</a>
+      </li>
+      </ul>
+      </nav>
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo"/>
-      </header>
-    </div>
+    <>
+    {header}
+      <h1>Homepage</h1>
+      <p>This is the homepage.</p>
+    </>
+       
   );
 }
 
