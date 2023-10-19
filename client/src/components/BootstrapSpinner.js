0cdfbf01489d22cb8feb7709f8ef7ea9ab00a19a
Filippa Hansen <filippahansen@Filippas-MacBook-Pro.local>
2023-10-20 00:12:06 +0200

    added spinner to employees

--- client/src/components/BootstrapSpinner.js
+++ client/src/components/BootstrapSpinner.js
@@ -2,6 +2,7 @@ export default function BootstrapSpinner() {
     return (
         <div className="d-flex justify-content-center">
             <div className="spinner-border" role="status">
+                <span className="visually-hidden">Loading...</span>
             </div>
         </div>
     );
--- client/src/pages/Company.js
+++ client/src/pages/Company.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import "./Pages.css";
+import BootstrapSpinner from "../components/BootstrapSpinner";
 
 export default function Company() {
     const [employees, setEmployees] = useState([]);
@@ -15,28 +16,31 @@ export default function Company() {
     return (
         <div className="content">
             <h2 className="home-center">Employee Information</h2>
+            {employees ? (
+                <ul className="employee-list">
+                    {Object.values(employees).map((employee) => (
+                        <li key={employee.id} className={`employee-list-item ${employee.hovered ? "hovered" : ""}`}>
+                            <div className={`employee-info ${employee.hovered ? "hovered" : ""}`}>
+                                <h4>{employee.name}</h4>
+                                <p>Employee ID: {employee.id}</p>
+                                <p>Total Sales Amount: {employee.totalSalesAmount}</p>
+                                <p>Cars Sold:
+                                    <ul>
+                                        {employee.carsSold.map((car, index) => (
+                                            <li key={index}>
+                                                {car.brand} {car.model} - {car.price} kr
+                                            </li>
+                                        ))}
+                                    </ul>
+                                </p>
+                            </div>
 
-            <ul className="employee-list">
-                {Object.values(employees).map((employee) => (
-                    <li key={employee.id} className={`employee-list-item ${employee.hovered ? "hovered" : ""}`}>
-                        <div className={`employee-info ${employee.hovered ? "hovered" : ""}`}>
-                            <h4>{employee.name}</h4>
-                            <p>Employee ID: {employee.id}</p>
-                            <p>Total Sales Amount: {employee.totalSalesAmount}</p>
-                            <p>Cars Sold:
-                                <ul>
-                                    {employee.carsSold.map((car, index) => (
-                                        <li key={index}>
-                                            {car.brand} {car.model} - {car.price} kr
-                                        </li>
-                                    ))}
-                                </ul>
-                            </p>
-                        </div>
-
-                    </li>
-                ))}
-            </ul>
+                        </li>
+                    ))}
+                </ul>
+            ) : (
+                <BootstrapSpinner />
+            )}
         </div>
     );
 }
